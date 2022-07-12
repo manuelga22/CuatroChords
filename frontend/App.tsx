e1abb2026ad969e52e8a7561c41bf17d09d00254
Manuel Garcia <manuelisaacgarcia@gmail.com>
2022-07-12 13:47:46 -0400

    added icons

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,10 +4,12 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './components/HomeScreen';
 import SettingsScreen from './components/SettingsScreen';
 import ChordViewComponent from './components/ViewChord/ChordFretboardView';
+
 
 const  HomeStack = createStackNavigator();
 const SettingsStack = createStackNavigator();
@@ -32,7 +34,23 @@ function SettingsScreenStack(){
 export default function App() {
   return (
       <NavigationContainer >
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Navigator         screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = "";
+
+            if (route.name === 'Home') {
+              iconName = focused? 'home': 'home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
           <Tab.Screen name="Home" component={HomeScreenStack} />
           <Tab.Screen name="Settings" component={SettingsScreenStack} />
         </Tab.Navigator>
--- frontend/components/HomeScreen.tsx
+++ frontend/components/HomeScreen.tsx
@@ -1,52 +1,78 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import FretboardComponent from './CuatroFretboard/Fretboard';
 
-const HomeScreen = (props:any)=> {
+const HomeScreen = (props: any) => {
     const [chord, onChangeChord] = React.useState("")
     const [chordName, setChordName] = React.useState("")
 
-    const goToChordViewScreen = ()=>{
-       onChangeChord(chord)
-       setChordName("C major")
-       props.navigation.navigate('ChordView', {chordName: chordName, notes: ['C', 'E', 'G']})
+    const goToChordViewScreen = () => {
+        onChangeChord(chord)
+        setChordName("C major")
+        props.navigation.navigate('ChordView', { chordName: chordName, notes: ['C', 'E', 'G'] })
     }
 
-    return(
-    <View>
-        <View style={styles.SearchBarcontainer}>
-            <SafeAreaView>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={onChangeChord}
-                  value={chord}
-                  placeholder="Try C M (C Major) or C m (C minor)"
-                  returnKeyType='search'
-                  onSubmitEditing={()=>goToChordViewScreen()}
-                />
-            </SafeAreaView>
+    return (
+        <View>
+            <View style={styles.container}>
+                <View style={styles.iconWrapper} ><Icon name="search" size={30} color="#900" style={styles.icon} /></View>
+                <View style={styles.SearchBarcontainer}>
+                    <SafeAreaView>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={onChangeChord}
+                            value={chord}
+                            placeholder="Try C M (C Major) or C m (C minor)"
+                            returnKeyType='search'
+                            onSubmitEditing={() => goToChordViewScreen()}
+                        />
+                    </SafeAreaView>
+                </View>
+            </View>
+            <FretboardComponent style={styles.fretboard} chords={[]}></FretboardComponent>
         </View>
 
-        <FretboardComponent chords={[]}></FretboardComponent>
-    </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        padding: 3,
+    },
     SearchBarcontainer: {
-      backgroundColor: '#fff',
-      borderRadius: 10,
-      height: 50,
-      margin: 12,
-      padding: 10,
+        flexGrow: 2,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        height: 50,
+        margin: 10,
+        padding: 10,
     },
-    input:{
+    fretboard: {
+    },
+    input: {
         height: '100%',
-        width:'100%',
+        width: '100%',
     },
+    iconWrapper:{
+        backgroundColor: 'lightblue',
+        borderRadius: 5,
+        borderWidth:1,
+        height: 50,
+        width: '12%',
+        padding: 5,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    icon: {
+        color: 'white',
+    }
+
 });
 
 export default HomeScreen;
